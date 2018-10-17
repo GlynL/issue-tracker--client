744b5aa80c8294e82090eefcdc306ee2c6ccebf5
Glyn L <glyn_92@hotmail.com>
2018-10-17 23:37:09 +1000

    working on refactor

---
+++ src/components/Form.js
@@ -0,0 +1,153 @@
+import React, { Component } from "react";
+
+const defaultIssue = {
+  _id: "",
+  issue_title: "",
+  issue_text: "",
+  created_by: "",
+  assigned_to: "",
+  status_text: "",
+  created_on: "",
+  updated_on: "",
+  open: null
+};
+
+class Form extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      project: "",
+      filter: defaultIssue
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {}
+
+  componentDidUpdate(prevProps) {
+    if (prevProps !== this.props) {
+      this.setState({ project: this.props.project, filter: this.props.issue });
+    }
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    const { project, filter } = this.state;
+    this.props.formAction(project, filter);
+  }
+
+  handleChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    if (name === "project") return this.setState({ [name]: value });
+    const filter = { ...this.state.filter, [name]: value };
+    this.setState({ filter });
+  }
+
+  render() {
+    const { project, filter } = this.state;
+    const readOnly = this.props.type === "edit" ? true : false;
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            name="project"
+            type="text"
+            placeholder="Project name"
+            onChange={this.handleChange}
+            value={project}
+            required
+            readOnly={readOnly}
+          />
+          <input
+            name="_id"
+            type="text"
+            placeholder="_id"
+            onChange={this.handleChange}
+            value={filter._id}
+            readOnly={readOnly}
+          />
+          <input
+            name="issue_title"
+            type="text"
+            placeholder="title"
+            onChange={this.handleChange}
+            value={filter.issue_title}
+          />
+          <input
+            name="issue_text"
+            type="text"
+            placeholder="text"
+            onChange={this.handleChange}
+            value={filter.issue_text}
+          />
+          <input
+            name="created_by"
+            type="text"
+            placeholder="created_by"
+            onChange={this.handleChange}
+            value={filter.created_by}
+          />
+          <input
+            name="created_by"
+            type="text"
+            placeholder="created_by"
+            onChange={this.handleChange}
+            value={filter.created_by}
+          />
+          <input
+            name="assigned_to"
+            type="text"
+            placeholder="assigned_to"
+            onChange={this.handleChange}
+            value={filter.assigned_to}
+          />
+          <input
+            name="status_text"
+            type="text"
+            placeholder="status_text"
+            onChange={this.handleChange}
+            value={filter.status_text}
+          />
+          <input
+            name="created_on"
+            type="text"
+            placeholder="created_on"
+            onChange={this.handleChange}
+            value={filter.created_on}
+          />
+          <input
+            name="updated_on"
+            type="text"
+            placeholder="updated_on"
+            onChange={this.handleChange}
+            value={filter.updated_on}
+          />
+          <input
+            id="open"
+            name="open"
+            type="radio"
+            placeholder="open"
+            onChange={this.handleChange}
+            value={true}
+          />
+          <label htmlFor="open">open</label>
+          <input
+            id="closed"
+            name="open"
+            type="radio"
+            placeholder="closed"
+            onChange={this.handleChange}
+            value={false}
+          />
+          <label htmlFor="closed">closed</label>
+          <button>Submit</button>
+        </form>{" "}
+      </div>
+    );
+  }
+}
+
+export default Form;
--- src/containers/edit-issue.js
+++ src/containers/edit-issue.js
@@ -2,35 +2,21 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { bindActionCreators } from "redux";
 import { fetchIssues, updateIssue } from "../actions/index";
-
-const defaultIssue = {
-  _id: "",
-  issue_title: "",
-  issue_text: "",
-  created_by: "",
-  assigned_to: "",
-  status_text: "",
-  created_on: "",
-  updated_on: "",
-  open: null
-};
+import Form from "../components/Form";
 
 class EditIssue extends Component {
   constructor(props) {
     super(props);
     this.state = {
       project: "",
-      filter: defaultIssue
+      issue: {}
     };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
   async componentDidMount() {
     const { project, id } = this.props.match.params;
     await this.props.fetchIssues(project, { _id: id });
-    this.setState({ project, filter: this.props.issues[0] });
+    this.setState({ project, issue: this.props.issues[0] });
   }
 
   handleSubmit(e) {
@@ -49,102 +35,15 @@ class EditIssue extends Component {
 
   render() {
     if (this.props.issues.length === 0) return <div>Loading...</div>;
-    const { project, filter } = this.state;
     return (
       <div>
         <div>edit issue</div>
-        <form onSubmit={this.handleSubmit}>
-          <input
-            name="project"
-            type="text"
-            placeholder="Project name"
-            onChange={this.handleChange}
-            value={project}
-            required
-          />
-          <input
-            name="_id"
-            type="text"
-            placeholder="_id"
-            onChange={this.handleChange}
-            value={filter._id}
-          />
-          <input
-            name="issue_title"
-            type="text"
-            placeholder="title"
-            onChange={this.handleChange}
-            value={filter.issue_title}
-          />
-          <input
-            name="issue_text"
-            type="text"
-            placeholder="text"
-            onChange={this.handleChange}
-            value={filter.issue_text}
-          />
-          <input
-            name="created_by"
-            type="text"
-            placeholder="created_by"
-            onChange={this.handleChange}
-            value={filter.created_by}
-          />
-          <input
-            name="created_by"
-            type="text"
-            placeholder="created_by"
-            onChange={this.handleChange}
-            value={filter.created_by}
-          />
-          <input
-            name="assigned_to"
-            type="text"
-            placeholder="assigned_to"
-            onChange={this.handleChange}
-            value={filter.assigned_to}
-          />
-          <input
-            name="status_text"
-            type="text"
-            placeholder="status_text"
-            onChange={this.handleChange}
-            value={filter.status_text}
-          />
-          <input
-            name="created_on"
-            type="text"
-            placeholder="created_on"
-            onChange={this.handleChange}
-            value={filter.created_on}
-          />
-          <input
-            name="updated_on"
-            type="text"
-            placeholder="updated_on"
-            onChange={this.handleChange}
-            value={filter.updated_on}
-          />
-          <input
-            id="open"
-            name="open"
-            type="radio"
-            placeholder="open"
-            onChange={this.handleChange}
-            value={true}
-          />
-          <label htmlFor="open">open</label>
-          <input
-            id="closed"
-            name="open"
-            type="radio"
-            placeholder="closed"
-            onChange={this.handleChange}
-            value={false}
-          />
-          <label htmlFor="closed">closed</label>
-          <button>Submit</button>
-        </form>{" "}
+        <Form
+          type="edit"
+          issue={this.state.issue}
+          formAction={this.props.updateIssue}
+          project={this.state.project}
+        />
       </div>
     );
   }
